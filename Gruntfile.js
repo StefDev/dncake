41053580e41ccdc2f92679f6d2c203e542765164
Stef Krie <git@skww.de>
2013-12-17 23:36:55 +0100

    Initital Grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    cssmin: {
+      options: {
+        banner: "/* dncake css */"
+      },
+      minify: {
+        src: 'app/webroot/css/darkneuss.css',
+        dest: 'app/webroot/css/min/darkneuss.min.css'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "cssmin" task.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  
+  // Default tasks
+  grunt.registerTask('default', ['cssmin']);
+}
